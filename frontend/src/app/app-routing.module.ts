731244e307351af63a55b219720d5912145d704d
khan-alamin <mdalaminkhan367@gmail.com git config --global user.email>
2023-10-17 13:51:50 +0600

    frontend added

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FormPageComponent } from './form-page/form-page.component';
-import { ListPageComponent } from './list-page/list-page.component';
 
 const routes: Routes = [
-  {path: '', component: FormPageComponent},
-  {path: 'list', component: ListPageComponent},
+  // {path: '', component: FormPageComponent},
+  // {path: 'list', component: ListPageComponent},
 ];
 
 @NgModule({
