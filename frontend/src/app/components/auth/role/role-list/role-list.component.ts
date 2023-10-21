69b8f876000a26ea8717e09a9c574ba54d2a1e73
khan-alamin <mdalaminkhan367@gmail.com git config --global user.email>
2023-10-21 14:01:38 +0600

    customer data input

--- frontend/src/app/components/auth/role/role-list/role-list.component.ts
+++ frontend/src/app/components/auth/role/role-list/role-list.component.ts
@@ -1,23 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Role } from '../../../../model/auth/role';
 import { DataService } from 'src/app/services/data.service';
 import { Router } from '@angular/router';
+import { Permission } from 'src/app/model/auth/permission';
+import { Page } from '../../../../dto/page.dto';
 
 @Component({
   selector: 'app-role-list',
   templateUrl: './role-list.component.html',
   styleUrls: ['./role-list.component.scss']
 })
-export class RoleListComponent {
+export class RoleListComponent implements OnInit {
 
-  displayedColumns: string[] = ['name',  'permissions', 'actions'];
+  displayedColumns: string[] = ['name', 'permissions', 'actions'];
   dataSource: Role[] = [];
 
   constructor(private service: DataService, private router: Router) { }
 
+  ngOnInit(): void {
+    this.service.getList('customer').then((res: Page) => {
+      this.dataSource = res.data.content
+    }
+    );
+  }
+
   delete(index: number) {
     let id = this.dataSource[index].id as number;
-    this.service.delete(id,"role").subscribe(() => {
+    this.service.delete(id, "role").subscribe(() => {
       const newData = this.dataSource.filter((s, i) => i != index);
       this.dataSource = newData;
     })
@@ -26,6 +35,10 @@ export class RoleListComponent {
   edit(index: number) {
     this.service.data = { ...this.dataSource[index] };
     this.router.navigate(['/']);
+  }
+
+  getPermissionAsList(permissions: Permission[]): string {
+    return permissions.map((p: Permission) => p.name).join(", ");
   }
 
 }
