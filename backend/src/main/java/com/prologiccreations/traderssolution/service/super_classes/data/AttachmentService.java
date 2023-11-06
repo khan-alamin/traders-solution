8ecf4f295d9d812a14e0f4410af33043af227c66
khan-alamin <mdalaminkhan367@gmail.com>
2023-11-06 22:11:08 +0600

    attachment deleted backend

--- backend/src/main/java/com/prologiccreations/traderssolution/service/super_classes/data/AttachmentService.java
+++
@@ -1,9 +0,0 @@
-package com.prologiccreations.traderssolution.service.super_classes.data;
-
-import com.prologiccreations.traderssolution.model.config.Customer;
-import com.prologiccreations.traderssolution.model.data.Attachment;
-import com.prologiccreations.traderssolution.service.super_classes.CrudService;
-
-public interface AttachmentService extends CrudService<Attachment, Long> {
-
-}
